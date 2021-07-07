781e679645939c00b99fa10347435f801c718d41
Nicole Mellert <nicolemellert@Nicoles-MBP.localdomain>
2021-07-07 09:47:54 -0400

    Adds contents of new XCode project

---
+++ Shared/HealthViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthViewApp.swift
+//  Shared
+//
+//  Created by Nicole Mellert on 7/7/21.
+//
+
+import SwiftUI
+
+@main
+struct HealthViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
