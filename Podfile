755a2d4928193a73140152ca574cf344aeabe38c
Nicole Mellert <nmellert@nd.edu>
2021-07-07 13:42:38 -0400

    - Build podfile - Made an edit - In Pods AlamoFire target, moved 'Compile Sources' to before 'Headers' to fix build error

---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :ios, '12.0'
+
+target 'HealthView (iOS)'
+pod 'Alamofire', '~> 5.4'
+# pod 'AFNetworking', '~> 1.0'
--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -6,16 +6,33 @@
 //
 
 import SwiftUI
+import Alamofire
+import UIKit
+
+let apiNYT = "https://api.nytimes.com/svc/topstories/v2/health.json?api-key=eAfQI3BIAUG1Cja4EZvUg2j7K2Vm2jFS"
+
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+   var body: some View {
+      Text("Hello, world!")
+        .padding()
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    AF.request("https://api.nytimes.com/svc/topstories/v2/health.json").response { response in
+            debugPrint(response)
+        }
+    }
+}
+
